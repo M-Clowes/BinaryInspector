99ef3ab45997c0e566d3c3c9369a52b79bf46ec3
Matt Clowes <M-Clowes@noreply.github.com>
2026-05-20 10:38:55 +0100

    Added paging feature, fixed visual bug in the table UI and corrected non-conventional naming

--- BinaryInspector.cs
+++ BinaryInspector.cs
@@ -7,62 +7,89 @@ public static class BinaryInspector
     private static readonly char[] _hexLookup = CreateHexLookup();
     private static readonly char[] _asciiLookup = CreateAsciiLookup();
 
-    public static void PrintBytes(string fileName)
+    public static void PrintAllBytes(string fileName)
     {
         if (!File.Exists(fileName))
             throw new FileNotFoundException($"Unable to find file at: { fileName }");
-        byte[] fileData = File.ReadAllBytes(fileName);
+        long fileSize = new FileInfo(fileName).Length;
+        long maxPageCount    = (fileSize + PAGE_SIZE - 1) / PAGE_SIZE;
+        long pageNum         = 0;
+
+        using var stream = File.OpenRead(fileName); 
+
+        ConsoleKeyInfo input;
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine($"{ TABLE_COLOR }\u250c{ offsetBar }\u252c{ bytesBar }\u252c{ asciiBar }\u2510");
+            Console.WriteLine(
+                $"{ straightBar } "                                      +
+                $"{ OFFSET_COLOR }{ "Offset".PadRight(OFFSET_LENGTH) } " +
+                $"{ straightBar } "                                      +
+                $"{ BYTES_COLOR }{ "Bytes".PadRight(BYTES_LENGTH) } "    +
+                $"{ straightBar } "                                      +
+                $"{ ASCII_COLOR }{ "ASCII".PadRight(ASCII_LENGTH) } "    +
+                $"{ straightBar }"
+            );
+            Console.WriteLine($"{ TABLE_COLOR }\u251c{ offsetBar }\u253c{ bytesBar }\u253c{ asciiBar }\u2524");
         
-        // constants
-        const string resetColor     = "\x1b[0m";
-        const string offsetColor    = "\x1b[36m";
-        const string bytesColor     = "\x1b[97m";
-        const string nullBytesColor = "\x1b[90m";
-        const string asciiColor     = "\x1b[32m";
-        const string tableColor     = "\x1b[34m";
+            PrintBytesPage(stream, pageNum);
 
-        const int offsetLength = 8;
-        const int bytesLength = 48;
-        const int asciiLength = 16;
+            Console.WriteLine($"{ TABLE_COLOR }\u2514{ offsetBar }\u2534{ bytesBar }\u2534{ asciiBar }\u2518");
+            Console.WriteLine($"{ RESET_COLOR }<={ NULL_BYTES_COLOR } Page { pageNum + 1 } / { maxPageCount } (ESC to exit) { RESET_COLOR }=>");
 
-        const long maxBuf = 300_000;
+            while (true)
+            {
+                input = Console.ReadKey(true);
 
-        // vars
-        var offsetBar   = new string('\u2500', offsetLength + 2);
-        var bytesBar    = new string('\u2500', bytesLength + 2);
-        var asciiBar    = new string('\u2500', asciiLength + 2);
-        var straightBar = $"{ tableColor }\u2502";
+                if (input.Key == ConsoleKey.LeftArrow ||
+                    input.Key == ConsoleKey.A         ||
+                    input.Key == ConsoleKey.B         ||
+                    input.Key == ConsoleKey.W)
+                {
+                    pageNum = Math.Max(pageNum - 1, 0);
+                    break;
+                }
+                else if (input.Key == ConsoleKey.RightArrow ||
+                         input.Key == ConsoleKey.D          ||
+                         input.Key == ConsoleKey.N          ||
+                         input.Key == ConsoleKey.S)
+                {
+                    pageNum = Math.Min(pageNum + 1, maxPageCount - 1);
+                    break;
+                }
+                else if (input.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        } while (input.Key != ConsoleKey.Escape);
+    }
 
-        // header
-        Console.WriteLine($"{ tableColor }\u250c{ offsetBar }\u252c{ bytesBar }\u252c{ asciiBar }\u2510");
-        Console.WriteLine(
-            $"{ straightBar } "                                    +
-            $"{ offsetColor }{ "Offset".PadRight(offsetLength) } " +
-            $"{ straightBar } "                                    +
-            $"{ bytesColor }{ "Bytes".PadRight(bytesLength) } "    +
-            $"{ straightBar } "                                    +
-            $"{ asciiColor }{ "ASCII".PadRight(asciiLength) } "    +
-            $"{ straightBar }"
-        );
-        Console.WriteLine($"{ tableColor }\u251c{ offsetBar }\u2502{ bytesBar }\u2502{ asciiBar }\u2524");
+    private static void PrintBytesPage(FileStream stream, long pageNum)
+    {
+        var fileData = new byte[PAGE_SIZE];
 
-        // body
-        var ascii = new char[asciiLength];
+        stream.Seek(pageNum * PAGE_SIZE, SeekOrigin.Begin);
+        int bytesRead = stream.Read(fileData, 0, fileData.Length);
+
+        var ascii = new char[ASCII_LENGTH];
         var sb    = new StringBuilder();
 
         Span<char> buf = stackalloc char[8];
-        for (var i = 0; i < fileData.Length; i += asciiLength)
+        for (var i = 0; i < bytesRead; i += ASCII_LENGTH)
         {
             i.TryFormat(buf, out _, "X8");
-            sb.Append($"{ straightBar } { offsetColor }{ buf } { straightBar } ");
+            sb.Append($"{ straightBar } { OFFSET_COLOR }{ buf } { straightBar } ");
 
-            for (var j = 0; j < asciiLength; ++j)
+            for (var j = 0; j < ASCII_LENGTH; ++j)
             {
                 var idx = i + j;
 
                 if (idx < fileData.Length)
                 {
-                    sb.Append(fileData[idx] != 0x00 ? bytesColor : nullBytesColor);
+                    sb.Append(fileData[idx] != 0x00 ? BYTES_COLOR : NULL_BYTES_COLOR);
                     sb.Append(_hexLookup, fileData[idx] * 2, 2);
 
                     ascii[j] = _asciiLookup[fileData[idx]];
@@ -79,19 +106,18 @@ public static class BinaryInspector
                     sb.Append(' ');
             }
 
-            sb.Append($"{ straightBar } { asciiColor }{ ascii.AsSpan() } ");
+            sb.Append($"{ straightBar } { ASCII_COLOR }{ ascii.AsSpan() } ");
             sb.AppendLine(straightBar);
 
-            if (sb.Length >= maxBuf)
+            if (sb.Length >= MAX_BUF)
             {
                 Console.Write(sb.ToString());
                 sb.Clear();
             }
         }
-
-        // footer
-        sb.AppendLine($"{ tableColor }\u2514{ offsetBar }\u2534{ bytesBar }\u2534{ asciiBar}\u2518{ resetColor }");
-        Console.Write(sb.ToString());
+        
+        if (sb.Length > 0)
+            Console.Write(sb.ToString());
     }
 
     private static char[] CreateHexLookup()
@@ -116,4 +142,23 @@ public static class BinaryInspector
 
         return table;
     }
+
+    private const string RESET_COLOR      = "\x1b[0m";
+    private const string OFFSET_COLOR     = "\x1b[36m";
+    private const string BYTES_COLOR      = "\x1b[97m";
+    private const string NULL_BYTES_COLOR = "\x1b[90m";
+    private const string ASCII_COLOR      = "\x1b[32m";
+    private const string TABLE_COLOR      = "\x1b[34m";
+
+    private const int OFFSET_LENGTH = 8;
+    private const int BYTES_LENGTH  = 48;
+    private const int ASCII_LENGTH  = 16;
+
+    private const long MAX_BUF = 300_000;
+    private const long PAGE_SIZE = 32_000;
+
+    private static readonly string offsetBar   = new('\u2500', OFFSET_LENGTH + 2);
+    private static readonly string bytesBar    = new('\u2500', BYTES_LENGTH + 2);
+    private static readonly string asciiBar    = new('\u2500', ASCII_LENGTH + 2);
+    private static readonly string straightBar = $"{ TABLE_COLOR }\u2502";
 }
